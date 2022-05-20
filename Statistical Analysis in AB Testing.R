04db5a47fd7cd3618486eb3015cbb04185cfe6f4
QiutingWang <96046280+QiutingWang@users.noreply.github.com>
2022-05-20 19:25:41 +0800

    Add files via upload

---
+++ Statistical Analysis in AB Testing.R
@@ -0,0 +1,20 @@
+####Statistical Analysis in A/B Testing####
+
+###Power Analysis###
+##Definitions dive deeper:
+#Power: 
+#the probability of rejecting the null hypothesis when it's false;
+#the basic step of estimating the sample size needed to detect an effect of a particular magnitude; 
+#the test with higher power is preferred.
+
+#Significant Level:
+#the level of probability at which it is agreed that the null hypothesis will be rejected
+
+#Effect Size: the magnitude of the effect you are expecting
+#Difference between control and experiment groups population means of a response variable, divided by assumed common population standard deviation
+
+#Power Analysis Relationships: holding the significant level and effect size constant
+
+
+
+
